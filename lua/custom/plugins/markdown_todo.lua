acf812e17be7b7f25623965a036fe0b79daa468a
Pepe Gonzalez <apotafiy7@gmail.com>
2026-05-05 23:55:19 -0700

    Update markdown_todo to have markdown todo/checkbox creation and toggling on 'enter' key

--- lua/custom/plugins/markdown_todo.lua
+++ lua/custom/plugins/markdown_todo.lua
@@ -26,6 +26,52 @@ return {
             vim.api.nvim_feedkeys(keys, 'n', false)
           end
         end, { buffer = args.buf })
+
+        -- Normal mode Enter key: toggle existing todo or create new one
+        vim.keymap.set('n', '<CR>', function()
+          local line = vim.api.nvim_get_current_line()
+          -- Check if line starts with a todo checkbox (with optional indentation)
+          local indent, checkbox = line:match '^(%s*)(- %[.%])'
+          
+          if checkbox then
+            -- Toggle between "- [ ]" and "- [x]"
+            local new_checkbox
+            if checkbox == '- [ ]' then
+              new_checkbox = '- [x]'
+            elseif checkbox == '- [x]' then
+              new_checkbox = '- [ ]'
+            else
+              -- For any other checkbox state (like "- [~]"), default to "- [ ]"
+              new_checkbox = '- [ ]'
+            end
+            
+            -- Keep rest of the line content after checkbox
+            local rest_of_line = line:match '%- %[.%](.*)$'
+            if rest_of_line == nil then
+              rest_of_line = ''
+            end
+            
+            local new_line = indent .. new_checkbox .. rest_of_line
+            vim.api.nvim_set_current_line(new_line)
+            
+            -- Move cursor to end of checkbox
+            local cursor_row = vim.api.nvim_win_get_cursor(0)[1]
+            local cursor_col = #(indent .. new_checkbox)
+            vim.api.nvim_win_set_cursor(0, { cursor_row, cursor_col })
+          else
+            -- No checkbox found, prepend with "- [ ]"
+            local trim_line = line:gsub('^%s*', '')
+            local indent_match = line:match '^(%s*)'
+            local indent_str = indent_match or ''
+            local new_line = indent_str .. '- [ ] ' .. trim_line
+            vim.api.nvim_set_current_line(new_line)
+            
+            -- Move cursor to end of checkbox
+            local cursor_row = vim.api.nvim_win_get_cursor(0)[1]
+            local cursor_col = #(indent_str .. '- [ ] ')
+            vim.api.nvim_win_set_cursor(0, { cursor_row, cursor_col })
+          end
+        end, { buffer = args.buf })
       end,
     })
   end,
