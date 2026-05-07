b0e10c231e31a8bff15ddc9ade9cd434ce4a4235
Pepe Gonzalez <apotafiy7@gmail.com>
2026-05-06 22:28:22 -0700

    Update vim_markdown plugin to fix folding and some other adjustments

--- lua/custom/plugins/vim_markdown.lua
+++ lua/custom/plugins/vim_markdown.lua
@@ -4,6 +4,17 @@ return {
     'godlygeek/tabular',
   },
   init = function()
-    vim.g.vim_markdown_folding_disabled = 1
+    -- Enable folding but not auto-folding (foldmethod=manual)
+    vim.g.vim_markdown_folding_disabled = 0
+    vim.g.vim_markdown_folding_style_pythonic = 1
+    -- Fold H3 and higher (with pythonic style, level 3 = start folding at H3)
+    vim.g.vim_markdown_folding_level = 2
+
+    -- Conceal options
+    vim.g.vim_markdown_conceal = 1
+    vim.g.vim_markdown_conceal_code_blocks = 0
+
+    -- Table formatting with tabular
+    vim.g.vim_markdown_toc_autofit = 1 -- Auto-fit table of contents
   end,
 }
