a84a1ada88d84e06e7fd2190989a332468f6eb80
Pepe Gonzalez <apotafiy7@gmail.com>
2026-05-06 21:18:31 -0700

    Add catppuccin and everforest themes

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,61 @@
+return {
+  -- 'catppuccin/nvim',
+  -- name = 'catppuccin',
+  -- priority = 1000, -- Make sure it's loaded early
+  -- config = function()
+  --   require('catppuccin').setup {
+  --     flavour = 'mocha', -- latte, frappe, macchiato, mocha
+  --     background = {
+  --       light = 'latte',
+  --       dark = 'mocha',
+  --     },
+  --     transparent_background = true,
+  --     show_end_of_buffer = false,
+  --     term_colors = true,
+  --     dim_inactive = {
+  --       enabled = false,
+  --       shade = 'dark',
+  --       percentage = 0.15,
+  --     },
+  --     no_italic = false,
+  --     no_bold = false,
+  --     no_underline = false,
+  --     styles = {
+  --       comments = { 'italic' },
+  --       conditionals = { 'italic' },
+  --       loops = {},
+  --       functions = {},
+  --       keywords = { 'bold' },
+  --       strings = {},
+  --       variables = {},
+  --       numbers = {},
+  --       booleans = {},
+  --       properties = {},
+  --       types = {},
+  --       operators = {},
+  --     },
+  --     color_overrides = {},
+  --     custom_highlights = {},
+  --     integrations = {
+  --       cmp = true,
+  --       gitsigns = true,
+  --       nvimtree = true,
+  --       telescope = true,
+  --       treesitter = true,
+  --       treesitter_context = true,
+  --       which_key = true,
+  --       -- For more plugins integrations see: https://github.com/catppuccin/nvim#integrations
+  --     },
+  --   }
+
+  --   -- Apply the colorscheme
+  --   vim.cmd.colorscheme 'catppuccin'
+
+  --   -- Manual highlight overrides for transparency
+  --   -- Cursor line must be transparent for theme transparency to work
+  --   vim.api.nvim_set_hl(0, 'CursorLine', {})
+
+  --   -- Visual selection - transparent background with visible text
+  --   vim.api.nvim_set_hl(0, 'Visual', { fg = '#cdd6f4', bold = true, italic = true })
+  -- end,
+}
---
+++ lua/custom/plugins/everforest.lua
@@ -0,0 +1,24 @@
+return {
+  -- 'sainnhe/everforest',
+  -- priority = 1000, -- Make sure it's loaded early
+  -- config = function()
+  --   -- Available options: 'hard', 'medium', 'soft'
+  --   vim.g.everforest_background = 'hard'
+  --   -- Available options: 'dark', 'light'
+  --   vim.g.everforest_better_performance = 1
+  --   -- Enable transparency (0=disabled, 1=enabled)
+  --   vim.g.everforest_transparent_background = 1
+  --   -- Disable italic for certain elements
+  --   vim.g.everforest_disable_italic_comment = 1
+
+  --   -- Apply the colorscheme
+  --   vim.cmd.colorscheme 'everforest'
+
+  --   -- Manual highlight overrides for transparency
+  --   -- Cursor line must be transparent for theme transparency to work
+  --   vim.api.nvim_set_hl(0, 'CursorLine', {})
+
+  --   -- Visual selection - transparent background with visible text
+  --   vim.api.nvim_set_hl(0, 'Visual', { fg = '#e6dfc8', bold = true, italic = true })
+  -- end,
+}
