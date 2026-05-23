316926771a5eb4862b792d9970736b1814dfe81c
Pepe Gonzalez <apotafiy7@gmail.com>
2026-05-23 16:29:06 -0700

    Confirm for operations that might fail due to unsaved changes

--- init.lua
+++ init.lua
@@ -166,6 +166,11 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter' }, {
 -- Set conceal level to allow some fancier markdown rendering
 vim.opt.conceallevel = 2
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 vim.keymap.set({ 'i', 'n', 'v' }, 'kj', '<Esc>', { noremap = true, silent = true })
